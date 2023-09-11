f6ce48c08b23a609240ff7a9f19065077479cf77
aidan <aidan.zhlai@gmail.com>
2023-09-10 23:02:02 -0500

    changes

---
+++ script.js
@@ -0,0 +1,33 @@
+let words = []
+await fetch("wordlist.txt").then(p => p.text()).then((text) => {
+    words = text.split("\r\n")
+})
+
+let acceptableWords = []
+
+newWord()
+console.log("hi")
+console.log(acceptableWords)
+function newWord() {
+    let wordStr = words[Math.floor(Math.random() * words.length)]
+    acceptableWords = wordStr.split(", ")
+    document.getElementById("audio").setAttribute("src", "audio/" + acceptableWords[0] + ".mp3")
+    console.log("hi")
+}
+
+document.getElementById("input").addEventListener("keyup", (event) => {
+    if (event.code !== "Enter") {
+        return;
+    }
+    let messageText = document.getElementById("input").value.replace("'", "’")
+    document.getElementById("input").value = ""
+    let label = document.getElementById("label")
+    if (acceptableWords.includes(messageText)) {
+        label.classList = []
+        label.innerText = "correct!"
+    } else {
+        label.classList = ["wrong"]
+        label.innerText = "Incorrect! the words were " + acceptableWords.join(", ")
+    }
+    newWord()
+});
